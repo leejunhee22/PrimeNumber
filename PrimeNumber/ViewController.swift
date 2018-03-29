e6ec9fca7a81e0cf5a6fcba62f503caaa3822b0e
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-03-29 12:24:24 +0900

    Good Luck

--- PrimeNumber/ViewController.swift
+++ PrimeNumber/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+      @IBOutlet weak var LB: UILabel!
+      @IBOutlet weak var TF: UITextField!
       override func viewDidLoad() {
             super.viewDidLoad()
             // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,29 @@ class ViewController: UIViewController {
             super.didReceiveMemoryWarning()
             // Dispose of any resources that can be recreated.
       }
-
-
+      @IBAction func BTNC(_ sender: UIButton) {
+            let number=Int(TF.text!)
+            var isPrime = true
+            
+            
+            if number != 1 && number != 2 {
+                  for i in 2 ..< number! {
+                        if number! % i == 0{
+                              isPrime = true
+                        }
+                  }
+                  if isPrime == false {
+                        LB.text="Prime number"
+                  } else {
+                        LB.text="Not Prime number"
+                  }
+                  // Do any additional setup after loading the view, typically from a nib.
+            }
+      }
+      
+      @IBAction func BTNR(_ sender: UIButton) {
+            TF.text=""
+      }
+      
 }
 
